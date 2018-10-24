2ce2e20639a751f767d823b2d42fe108363c995f
JZ4012 <Joshua_zhou1999@hotmail.com>
2018-10-24 23:38:32 +1000

    Further splash screen improments
    changed background colour, added in sections and animations for text. Also added in buttons and contents.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+//Fade animation for the arrow.
+$(window).scroll(function(){
+    $(".scrollIcon").css("opacity", 1 - $(window).scrollTop() / 500);
+  });
 
+//scroll to top function.
+function backToTop() {
+    $("html, body").animate({scrollTop:0}, 'swing');
+}
