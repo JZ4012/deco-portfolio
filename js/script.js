08e37346725a9c5977643b74520b1ff41e02bfe4
JZ4012 <Joshua_zhou1999@hotmail.com>
2018-10-25 03:47:36 +1000

    Added all sections and added content to about section
    Major update to the about section. Site now also has all other sections outlined.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(window).scroll(function(){
     $(".scrollIcon").css("opacity", 1 - $(window).scrollTop() / 500);
   });
 
-//scroll to top function.
+//animated scroll to top function.
 function backToTop() {
     $("html, body").animate({scrollTop:0}, 'swing');
 }
